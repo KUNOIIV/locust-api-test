feeeccdf80d4efc21ee6c478379d4d9b5aca4f98
KUNOIIV <dyankey@protonmail.com>
2025-12-10 19:48:48 +0000

    locustfile.py

---
+++ README.md
@@ -0,0 +1,6 @@
+                                    #Locust API load test
+
+This script runs a full CRUD (Create, Get, Update and Delete) load test. This is to measure RPS (Response Per Second), latency and error rate against jsonplaceholder.typicode.com. 
+Performance stays flat even past 5000 concurrent users, no throttling. 
+
+Note: endpoints are like bouncers. http://bin.org or io blocked me with 503 and 404 errors, this one didn't. Learned it and tested it.  
--- locustfile.py
+++ locustfile.py
@@ -1,6 +1,32 @@
-from locust import HttpUser, task
+
+from locust import HttpUser, task, between
+
 class WebsiteUser(HttpUser):
-    host = "https://httpbin.org"    # this line fixes the error
+    host = "https://jsonplaceholder.typicode.com"
+    wait_time = between(1, 3)
+
+    @task 
+    def create_user(self): # This creates a post to the endpoint. Simulates user signup by POSTing JSON to /posts
+        self.client.post(
+            "/posts",                                   
+            json={"title": "Sword", "body": "Warrior", "userId": 1},
+            name="POST /posts"
+        )
+
     @task
-    def load_test(self):
-        self.client.get("/delay/0")
+    def get_users(self): # Fetches list of users/posts
+        self.client.get("/posts", name="GET /posts")   
+
+    @task
+    def update_user(self): # Updates existing record
+        self.client.put(
+            "/posts/1",                                 
+            json={"title": "Updated Sword", "body": "Master Warrior", "userId": 1},
+            name="PUT /posts/1"
+        )
+
+    @task
+    def delete_user(self): # Deletes record ID 1
+        self.client.delete("/posts/1", name="DELETE /posts/1")
+
+    
